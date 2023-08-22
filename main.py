3621695f2e501981c54a085d030ab0e2910a0dd7
VimukthiMK <vimukthikulathilaka@gmail.com>
2023-08-22 20:49:29 +0530

    Created Sidebar for input

--- main.py
+++ main.py
@@ -21,5 +21,31 @@ df=pd.read_csv('./data/diabetes.csv')
 # Headings of app
 st.title('DiaPredictor -Live Diabetes Checker')
 
+# SideBar
+st.sidebar.header('Patient Data')
+st.sidebar.write('Please Adjust Your Values')
+
+# Sidebar Function
+def user_report():
+  pregnancies = st.sidebar.slider('Pregnancies', 0,20, 3 )
+  glucose = st.sidebar.slider('Glucose (mg/dL)', 0,250, 120 )
+  bp = st.sidebar.slider('Blood Pressure (mm Hg)', 0,180, 70 )
+  skinthickness = st.sidebar.slider('Skin Thickness (mm)', 0,100, 20 )
+  insulin = st.sidebar.slider('Insulin (μU/mL)', 0,1000, 79 )
+  bmi = st.sidebar.slider('BMI (kg/m²)', 0,67, 20 )
+  dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.5, 0.47 )
+  age = st.sidebar.slider('Age', 1,100, 33 )
+
+  user_report_data = {
+      'Pregnancies':pregnancies,
+      'Glucose':glucose,
+      'BP':bp,
+      'Skin Thickness':skinthickness, 
+      'Insulin':insulin,
+      'BMI':bmi,
+      'DPF':dpf,
+      'Age':age
+  }
+
 
 
